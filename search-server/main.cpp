15ff49721d9f2cb53d5642f58aeb33b1b7b83a18
ponchik327 <seva-enkin@mail.ru>
2023-03-07 13:58:35 +0300

    third sprint attempt two

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,3 +1,5 @@
+#include <numeric>
+
 void TestExcludeStopWordsFromAddedDocumentContent() {
     const int doc_id = 42;
     const string content = "cat in the city"s;
@@ -30,9 +32,13 @@ void TestMinusWords() {
         SearchServer server;
         server.AddDocument(doc_id_1, content_1, DocumentStatus::ACTUAL, ratings);
         server.AddDocument(doc_id_2, content_2, DocumentStatus::ACTUAL, ratings);
-        const auto found_docs = server.FindTopDocuments("cat -city"s);
+        auto found_docs = server.FindTopDocuments("cat -city"s);
         ASSERT_EQUAL(found_docs.size(), 1);
-        const Document& doc0 = found_docs[0];
+        Document doc0 = found_docs[0];
+        ASSERT_EQUAL(doc0.id, doc_id_2);
+        found_docs = server.FindTopDocuments("home"s);
+        ASSERT_EQUAL(found_docs.size(), 1);
+        doc0 = found_docs[0];
         ASSERT_EQUAL(doc0.id, doc_id_2);
     }
 }
@@ -45,23 +51,33 @@ void TestMatchDocument() {
     const vector<int> ratings = {1, 2, 3};
     {
         SearchServer server;
+        server.SetStopWords("in the"s);
         server.AddDocument(doc_id_1, content_1, DocumentStatus::ACTUAL, ratings);
-        server.AddDocument(doc_id_2, content_2, DocumentStatus::ACTUAL, ratings);
+        server.AddDocument(doc_id_2, content_2, DocumentStatus::BANNED, ratings);
         vector<string> v = {"cat"s, "city"s}; 
         
         {
-            const auto [words, status] = server.MatchDocument("cat city"s, 42);
+            const auto [words, status] = server.MatchDocument("cat city"s, doc_id_1);
             ASSERT_EQUAL(words, v);
+            ASSERT(status == DocumentStatus::ACTUAL);
         }
 
         {
-            const auto [words, status] = server.MatchDocument("cat -home"s, 80);
+            const auto [words, status] = server.MatchDocument("cat -home"s, doc_id_2);
             ASSERT(words.empty());
+            ASSERT(status == DocumentStatus::BANNED);
         }
 
         {
-            const auto [words, status] = server.MatchDocument("home dog"s, 42);
+            const auto [words, status] = server.MatchDocument("home dog"s, doc_id_1);
             ASSERT(words.empty());
+            ASSERT(status == DocumentStatus::ACTUAL);
+        }
+        
+        {
+            const auto [words, status] = server.MatchDocument("in the"s, doc_id_1);
+            ASSERT(words.empty());
+            ASSERT(status == DocumentStatus::ACTUAL);
         }
     }
 }
@@ -82,10 +98,7 @@ void TestSortRelevance() {
         const auto found_docs = server.FindTopDocuments("cat city in"s);
         ASSERT_EQUAL(found_docs.size(), 3);
         ASSERT(found_docs[0].relevance > found_docs[1].relevance);
-        ASSERT(found_docs[1].relevance > found_docs[2].relevance);
-        for(int i = 0; i < found_docs.size(); ++i) {
-            ASSERT_EQUAL(found_docs[i].relevance, found_docs[i].relevance);
-        }    
+        ASSERT(found_docs[1].relevance > found_docs[2].relevance);   
     }
 }
 
@@ -105,9 +118,12 @@ void TestAverageRating() {
         server.AddDocument(doc_id_2, content_2, DocumentStatus::ACTUAL, ratings_2);
         server.AddDocument(doc_id_3, content_3, DocumentStatus::ACTUAL, ratings_3);
         const auto found_docs = server.FindTopDocuments("cat city in"s);
-        ASSERT_EQUAL(found_docs[0].rating, 2);
-        ASSERT_EQUAL(found_docs[1].rating, 0);
-        ASSERT_EQUAL(found_docs[2].rating, 0);
+        const int average_rating_1 = accumulate(ratings_1.begin(), ratings_1.end(), 0) / ratings_1.size();
+        const int average_rating_2 = 0;
+        const int average_rating_3 = 0;
+        ASSERT_EQUAL(found_docs[0].rating, average_rating_1);
+        ASSERT_EQUAL(found_docs[1].rating, average_rating_2);
+        ASSERT_EQUAL(found_docs[2].rating, average_rating_3);
     }
 }
 
@@ -182,10 +198,48 @@ void TestCorrectComputeRelevance() {
         server.AddDocument(doc_id_2, content_2, DocumentStatus::ACTUAL, ratings_2);
         server.AddDocument(doc_id_3, content_3, DocumentStatus::ACTUAL, ratings_3);
         const auto found_docs = server.FindTopDocuments("cat city in"s);
-        ASSERT(abs(found_docs[0].relevance - 0.376019) < 1e-6);
-        ASSERT(abs(found_docs[1].relevance - 0.101366) < 1e-6);
-        (found_docs[0].relevance == 0);
+        const double EPSILON = 1e-6;
+        const double relevance_1 = 0.376019;
+        const double relevance_2 = 0.101366;
+        const double relevance_3 = 0;
+        ASSERT(abs(found_docs[0].relevance - relevance_1) < EPSILON);
+        ASSERT(abs(found_docs[1].relevance - relevance_2) < EPSILON);
+        ASSERT(abs(found_docs[2].relevance - relevance_3) < EPSILON);
     }
+}
+
+void TestGetDocumentCount() {
+    const int doc_id_1 = 42;
+    const int doc_id_2 = 81;
+    const int doc_id_3 = 10;
+    const string content_1 = "cat in the city"s;
+    const string content_2 = "cat in the home"s;
+    const string content_3 = "dog in the home"s;
+    const vector<int> ratings_1 = {1, 2, 3};
+    const vector<int> ratings_2 = {-1, 2, 3};
+    const vector<int> ratings_3 = {};
+    {
+        SearchServer server; 
+        server.AddDocument(doc_id_1, content_1, DocumentStatus::ACTUAL, ratings_1);
+        server.AddDocument(doc_id_2, content_2, DocumentStatus::ACTUAL, ratings_2);
+        server.AddDocument(doc_id_3, content_3, DocumentStatus::ACTUAL, ratings_3);
+        const int count_doc = 3;
+        ASSERT_EQUAL(server.GetDocumentCount(), count_doc);
+    }
+}
+
+void TestAddDocument() {
+    const int doc_id = 42; 
+    const string content = "cat in the city"s;
+    const vector<int> ratings = {1, 2, 3};
+    {
+        SearchServer server; 
+        server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
+        const vector<Document> doc = server.FindTopDocuments("cat city in");
+        ASSERT_EQUAL(doc[0].id, doc_id); 
+        const int average_rating = accumulate(ratings.begin(), ratings.end(), 0) / ratings.size();
+        ASSERT_EQUAL(doc[0].rating, average_rating);
+    } 
 }
 
 void TestSearchServer() {
@@ -196,4 +250,6 @@ void TestSearchServer() {
     RUN_TEST(TestAverageRating);
     RUN_TEST(TestPredicatFunction);
     RUN_TEST(TestCorrectComputeRelevance);
+    RUN_TEST(TestGetDocumentCount);
+    RUN_TEST(TestAddDocument);
 }
